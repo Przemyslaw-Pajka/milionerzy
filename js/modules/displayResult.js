c1583931c29adcbc8a6681f38fdc66b50ce2b6ba
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2021-10-10 23:25:31 +0200

    fix problem with display wrong correct answer

--- js/modules/displayResult.js
+++ js/modules/displayResult.js
@@ -1,12 +1,15 @@
 import messages from './messages.js';
 import { getGameStateFromDB } from '../classes/GameState.js';
+import resultTypes from './resultTypes.js';
 
 const displayResult = async (resultType) => {
     const { correctAnswer } = await getGameStateFromDB();
     const gameWrapperInfo = $('.game-wrapper__info');
-    messages.looseGame += correctAnswer
-    const messageToDisplay = messages[resultType]
 
+    if(resultType === resultTypes.LOOSE_GAME)
+        messages.looseGame += correctAnswer
+
+    const messageToDisplay = messages[resultType]
     gameWrapperInfo.append(messageToDisplay)
 
 }
